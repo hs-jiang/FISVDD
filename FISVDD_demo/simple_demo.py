b086e9c3cd419cb3c7872f0ba736dd9fb258687e
Hansi <hsjiang2010@gmail.com>
2019-09-01 21:36:56 -0400

    demo update

--- FISVDD_demo/simple_demo.py
+++ FISVDD_demo/simple_demo.py
@@ -6,18 +6,33 @@ import time
 import pandas as pd
 
 
-data = pd.read_csv("banana_ol.csv", header=0)
+data = pd.read_csv("sample_input.csv", header=0)
 data = np.array(data)
 
-s = 0.8  # initialization with a random number between 0~1
+
+s = 0.8 # initialization with a random number between 0~1
 fd = fisvdd(data, s)
 fd.find_sv()
 fd._print_res()
 
-# plot result
-plt.scatter(data[:, 0], data[:, 1], s=30)
-plt.scatter(fd.sv[:, 0], fd.sv[:, 1], marker='*')
-plt.title('original data with detected boundary')
+#################
+#  plot result  #
+#################
+plt.scatter(data[:, 0], data[:, 1], s=18)
+plt.title('Original Data')
+plt.savefig('original_data', dpi=300)
+plt.show()
+plt.scatter(fd.sv[:, 0], fd.sv[:, 1], s=18)
+plt.scatter(fd.sv[:, 0], fd.sv[:, 1], marker='*', s=18)
+plt.title('Support Vectors')
+plt.savefig('support_vectors', dpi=300)
+plt.show()
+plt.scatter(data[:, 0], data[:, 1], s=18)
+plt.scatter(fd.sv[:, 0], fd.sv[:, 1], marker='*', s=18)
+plt.title('Original Data with Support Vectors')
+plt.savefig('final_result', dpi=300)
 plt.show()
 plt.plot(fd.obj_val)
+plt.title('Objective Function Value')
+plt.savefig('obv', dpi=300)
 plt.show()
